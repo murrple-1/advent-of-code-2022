ed901142aa67ff72bd7f8d223fc9da9c3774d9d9
Murray Christopherson <murraychristopherson@gmail.com>
2022-12-04 12:34:35 -0800

    day 4

---
+++ 4/main_1.py
@@ -0,0 +1,27 @@
+def main():
+    with open("input.txt", "r") as f:
+        overlaps = 0
+        for line in f.read().splitlines():
+            first_range_str, second_range_str = line.split(",")
+
+            first_range_start_str, first_range_end_str = first_range_str.split("-")
+            second_range_start_str, second_range_end_str = second_range_str.split("-")
+
+            first_range_start, first_range_end = int(first_range_start_str), int(
+                first_range_end_str
+            )
+            second_range_start, second_range_end = int(second_range_start_str), int(
+                second_range_end_str
+            )
+
+            first_range = frozenset(range(first_range_start, first_range_end + 1, 1))
+            second_range = frozenset(range(second_range_start, second_range_end + 1, 1))
+
+            if first_range.issubset(second_range) or second_range.issubset(first_range):
+                overlaps += 1
+
+        print(overlaps)
+
+
+if __name__ == "__main__":
+    main()
