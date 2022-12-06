f7f2b99879d5abeeadac1b75755c9be1600991e7
Murray Christopherson <murraychristopherson@gmail.com>
2022-12-06 11:06:13 -0800

    day 6

---
+++ 6/main_2.py
@@ -0,0 +1,18 @@
+def main():
+    with open("input.txt", "r") as f:
+        lines = f.read()
+
+    cs: list[str | None] = [None] * 14
+
+    for i, c in enumerate(lines):
+        cs.pop(0)
+        cs.append(c)
+
+        if all(cs):
+            if len(frozenset(cs)) == len(cs):
+                print(i + 1)
+                break
+
+
+if __name__ == "__main__":
+    main()
